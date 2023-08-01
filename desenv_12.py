93be48da14fdc6b3b835164a9f9b206a88c44bdb
Gilson Ferrer <gmferreira.eng@gmail.com>
2023-08-01 07:28:41 -0300

    lê nome e ano de nascimento com retorno de idade no ano atual

--- desenv_12.py
+++ desenv_12.py
@@ -5,18 +5,15 @@ from datetime import date
 current = date.today().year
 while True:
     try:
-        name = input('\nDigite seu nome: ').title().strip() 
-        year = input('Digite o ano de seu nascimento: ')
-        if int(year) < 1922 or int(year) > current - 1:
-            print(f'Por favor {name}, digite datas entre 1922 e {current -1}')
-            continue
-        if len(name) == 0:
-            print('Digite o seu nome por favor.')
+        name_ = input('\nDigite seu nome: ').title().strip() or 'usuário' 
+        year_ = input('Digite o ano de seu nascimento: ')
+        if int(year_) < 1922 or int(year_) > current - 1:
+            print(f'Por favor {name_}, digite datas entre 1922 e {current -1}')
             continue
     except Exception as error:
         print(error)
         continue
     break
 
-print(f'\nOlá {name}, nesse ano de {current} você completou/completará {current - int(year)} anos.')
+print(f'\nOlá {name_}, nesse ano de {current} você completou/completará {current - int(year_)} anos.')
 
